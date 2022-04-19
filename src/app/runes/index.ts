b9187995b83b8eb3c474c84cdb430fa7795cf210
Ajrarn <christophe.ajrarn@gmail.com>
2022-04-19 23:10:41 +1100

    feat: all runes as icon

--- src/app/runes/index.ts
+++ src/app/runes/index.ts
@@ -4,11 +4,115 @@ import { RuneMastery } from './rune-mastery';
 import { RuneMaker } from './rune-maker';
 import { RuneCommunicationTrade } from './rune-communication-trade';
 import { RuneMan } from './rune-man';
+import { RuneAir } from './rune-air';
+import { RuneBartar } from './rune-bartar';
+import { RuneBeast } from './rune-beast';
+import { RuneChaos } from './rune-chaos';
+import { RuneCold } from './rune-cold';
+import { RuneCombine } from './rune-combine';
+import { RuneCommand } from './rune-command';
+import { RuneDarkness } from './rune-darkness';
+import { RuneDeath } from './rune-death';
+import { RuneDisorder } from './rune-disorder';
+import { RuneDispel } from './rune-dispel';
+import { RuneDragon } from './rune-dragon';
+import { RuneDragonewt } from './rune-dragonewt';
+import { RuneEarth } from './rune-earth';
+import { RuneEternalBattle } from './rune-eternal-battle';
+import { RuneFertility } from './rune-fertility';
+import { RuneFireSky } from './rune-fire-sky';
+import { RuneFireSky2 } from './rune-fire-sky2';
+import { RuneGod } from './rune-god';
+import { RuneHarmony } from './rune-harmony';
+import { RuneHeler } from './rune-heler';
+import { RuneHell } from './rune-hell';
+import { RuneHorse } from './rune-horse';
+import { RuneIllusion } from './rune-illusion';
+import { RuneInfinity } from './rune-infinity';
+import { RuneLaw } from './rune-law';
+import { RuneLight } from './rune-light';
+import { RuneLightbringers } from './rune-lightbringers';
+import { RuneLuck } from './rune-luck';
+import { RuneMagic } from './rune-magic';
+import { RuneMalkion } from './rune-malkion';
+import { RuneMoonBlack } from './rune-moon-black';
+import { RuneMoonCrescentCome } from './rune-moon-crescent-come';
+import { RuneMoonCrescentGo } from './rune-moon-crescent-go';
+import { RuneMoonEmptyHalf } from './rune-moon-empty-half';
+import { RuneMoonFull } from './rune-moon-full';
+import { RuneMoonFullHalf } from './rune-moon-full-half';
+import { RuneMovementChange } from './rune-movement-change';
+import { RuneOdayla } from './rune-odayla';
+import { RunePamalt } from './rune-pamalt';
+import { RunePlant } from './rune-plant';
+import { RuneSartar } from './rune-sartar';
+import { RuneSeparate } from './rune-separate';
+import { RuneShargash } from './rune-shargash';
+import { RuneSpirit } from './rune-spirit';
+import { RuneStasis } from './rune-stasis';
+import { RuneSummon } from './rune-summon';
+import { RuneTap } from './rune-tap';
+import { RuneTruth } from './rune-truth';
+import { RuneUndeath } from './rune-undeath';
+import { RuneWater } from './rune-water';
+import { RuneYinkin } from './rune-yinkin';
 
 export const runes: IconDefinition[] = [
+  RuneAir,
+  RuneBartar,
+  RuneBeast,
+  RuneChaos,
+  RuneCold,
+  RuneCombine,
+  RuneCommand,
   RuneCommunicationTrade,
+  RuneDarkness,
+  RuneDeath,
+  RuneDisorder,
+  RuneDispel,
+  RuneDragon,
+  RuneDragonewt,
+  RuneEarth,
+  RuneEternalBattle,
   RuneFate,
+  RuneFertility,
+  RuneFireSky,
+  RuneFireSky2,
+  RuneGod,
+  RuneHarmony,
+  RuneHeler,
+  RuneHell,
+  RuneHorse,
+  RuneIllusion,
+  RuneInfinity,
+  RuneLaw,
+  RuneLight,
+  RuneLightbringers,
+  RuneLuck,
+  RuneMagic,
   RuneMaker,
+  RuneMalkion,
   RuneMan,
-  RuneMastery
+  RuneMastery,
+  RuneMoonBlack,
+  RuneMoonCrescentCome,
+  RuneMoonCrescentGo,
+  RuneMoonEmptyHalf,
+  RuneMoonFull,
+  RuneMoonFullHalf,
+  RuneMovementChange,
+  RuneOdayla,
+  RunePamalt,
+  RunePlant,
+  RuneSartar,
+  RuneSeparate,
+  RuneShargash,
+  RuneSpirit,
+  RuneStasis,
+  RuneSummon,
+  RuneTap,
+  RuneTruth,
+  RuneUndeath,
+  RuneWater,
+  RuneYinkin
 ];
