fc05784fcb872b6f95c37e39f1b67f2754ee5b8d
Ajrarn <christophe.ajrarn@gmail.com>
2022-05-16 21:05:39 +1100

    feat: specices with params

--- src/app/pages/species/species.component.ts
+++ src/app/pages/species/species.component.ts
@@ -5,6 +5,18 @@ import { ChangeTitle } from '../../store/title.action';
 import { Specie } from '../../store/models';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+export interface TreeNodeInterface {
+  name: string;
+  param?: string;
+  bonus?: number;
+  value?: number;
+  formula?: string;
+  level?: number;
+  expand?: boolean;
+  items?: TreeNodeInterface[];
+  parent?: TreeNodeInterface;
+}
+
 @Component({
   selector: 'app-species',
   templateUrl: './species.component.html',
@@ -53,16 +65,63 @@ export class SpeciesComponent {
         this.selectSpecie(id);
       }
     });
-}
+  }
 
   selectSpecie(specieName: string) {
     if (specieName) {
       let specie = this.species.find(item => item.name === specieName);
       if (specie) {
         this.specie = specie;
-        console.log('specie', this.specie);
+
+        this.specie.skills.forEach(item => {
+          this.mapOfExpandedData[item.name] = this.convertTreeToList(item);
+        });
+
+
       }
       this.store.dispatch(new ChangeTitle(specieName));
+    }
+  }
+
+  mapOfExpandedData: { [key: string]: TreeNodeInterface[] } = {};
+
+  collapse(array: TreeNodeInterface[], data: TreeNodeInterface, $event: boolean): void {
+    if (!$event) {
+      if (data.items) {
+        data.items.forEach(d => {
+          const target = array.find(a => a.name === d.name)!;
+          target.expand = false;
+          this.collapse(array, target, false);
+        });
+      } else {
+        return;
+      }
+    }
+  }
+
+  convertTreeToList(root: TreeNodeInterface): TreeNodeInterface[] {
+    const stack: TreeNodeInterface[] = [];
+    const array: TreeNodeInterface[] = [];
+    const hashMap = {};
+    stack.push({ ...root, level: 0, expand: false });
+
+    while (stack.length !== 0) {
+      const node = stack.pop()!;
+      this.visitNode(node, hashMap, array);
+      if (node.items) {
+        for (let i = node.items.length - 1; i >= 0; i--) {
+          stack.push({ ...node.items[i], level: node.level! + 1, expand: false, parent: node });
+        }
+      }
+    }
+
+    return array;
+  }
+
+  visitNode(node: TreeNodeInterface, hashMap: { [key: string]: boolean }, array: TreeNodeInterface[]): void {
+    if (!hashMap[node.name + node.param]) {
+      hashMap[node.name + node.param] = true;
+      array.push(node);
     }
   }
 
